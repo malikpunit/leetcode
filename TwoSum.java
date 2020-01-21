ab9bb0e1ba01bc38e0add9f66796b011f671165a
Punit Malik <malikpunit@ymail.com>
2020-01-22 01:39:03 +0530

    TwoSum Submitted.

---
+++ TwoSum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //return bruteForceWay(nums, target);
+        return anotherway(nums, target);
+    }
+    //O(N^2)
+    public int[] bruteForceWay(int []nums, int target){
+        int numsLength = nums.length;
+        int[] result  = new int[2];
+        for(int i=0; i<numsLength; i++){
+            for(int j = numsLength-1; j>i; j--){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }            
+            }
+        }
+        return new int[0];
+    }
+  
+    // O(N)
+    public int[] anotherway(int[] nums, int target){
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int numsLength = nums.length;
+        int complement = 0;
+        
+        for(int i = 0; i< numsLength; i++){
+            complement = target - nums[i];
+            if(numMap.containsKey(complement)){
+                int[] result = new int[2];
+                result[0] = i;
+                result[1] = numMap.get(complement);    
+                return result;
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
+    
+    
+}
